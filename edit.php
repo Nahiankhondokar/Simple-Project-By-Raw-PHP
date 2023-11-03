8a23a9061bf5e1a01cceccf47c2da005ecfb8a4a
Abdullah Al Nahian <info.nahian13@gmail.com>
2023-11-03 19:31:42 +0600

    user update

--- edit.php
+++ edit.php
@@ -35,34 +35,35 @@
 
     <?php
 
-    // if(isset($_POST['add'])){
+    if(isset($_POST['add'])){
 
-    //   // form info
-    //   $name = $_POST['name'];
-    //   $email = $_POST['email'];
-    //   $phone = $_POST['phone'];
-    //   $password = $_POST['password'];
+      // form info
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
 
-    //   // file upload
-    //   $image = $_FILES['image'];
-    //   $uniqueFileName = md5(time().rand()).$image['name'];
-    //   move_uploaded_file($image['tmp_name'], 'photo/'.$uniqueFileName);
+      $userId = $_GET['user_id'];
 
-    //   // validation
-    //   if(empty($name) || empty($email) || empty($phone) || empty($password)){
-    //       $message =  "<p style='color:red;'>Fields are required</p>";
-    //   }elseif(filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
-    //     $message =  "<p style='color:red;'>Invalid email</p>";
-    //   }elseif(filter_var($phone, FILTER_VALIDATE_INT) == false){
-    //     $message =  "<p style='color:red;'>Invalid number</p>";
-    //   }else {
-    //     $sql = "INSERT INTO users(name, phone, email, roll, image) VALUES ('$name', '$phone', '$email', 'Admin', '$uniqueFileName')";
-    //     $connection->query($sql); 
+      // file upload
+      $image = $_FILES['image'];
+      $uniqueFileName = md5(time().rand()).$image['name'];
+      move_uploaded_file($image['tmp_name'], 'photo/'.$uniqueFileName);
 
-    //     header('location:users.php');
-    //   }
+      // validation
+      if(empty($name) || empty($email) || empty($phone)){
+          $message =  "<p style='color:red;'>Fields are required</p>";
+      }elseif(filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
+        $message =  "<p style='color:red;'>Invalid email</p>";
+      }elseif(filter_var($phone, FILTER_VALIDATE_INT) == false){
+        $message =  "<p style='color:red;'>Invalid number</p>";
+      }else {
+        $sql = "UPDATE users SET name='$name', email='$email', phone='$phone', image='$uniqueFileName' WHERE id='$userId '";
+        $connection->query($sql); 
 
-    // }
+        header('location:users.php');
+      }
+
+    }
 
     ?>
 
@@ -90,13 +91,16 @@
                             <input type="text" name="email" placeholder="Email" value="<?php echo $user['email']; ?>" />
                         </div>
                         <div class="auth-form">
-                            <input type="text" name="phone" placeholder="Phone number" <?php echo $user['phone']; ?> />
+                            <input type="text" name="phone" placeholder="Phone number" value="<?php echo $user['phone']; ?>" />
                         </div>
+                        <img src="photo/<?php echo $user['image']; ?>" alt="" style="width: 100px; height: 100px;">
                         <div class="auth-form">
-                            <input type="file" name="image" <?php echo $user['image']; ?> />
+                            <input type="file" name="image" value="<?php echo $user['image']; ?>" />
                         </div>
                         <div class="auth-form">
                             <button name="add" type="submit">Update</button>
+                            <br><br>
+                            <a href="users.php" style="background-color: green; color: white;     padding: 5px 30px; border-radius: 5px; margin:auto;">User List</a>
                         </div>
                     </form>
 
